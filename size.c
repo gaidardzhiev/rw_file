6b6fb7487ee89ede041c21929038d5808ea37099
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2023-10-04 10:37:25 +0300

    Add files via upload

---
+++ size.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ERROR (-1)
+
+long size(char *filename) {
+	FILE *f = fopen(filename, "r");
+	if (f == NULL) {
+		return ERROR;
+	}
+	if (fseek(f, 0, SEEK_END) < 0) {
+		fclose(f);
+		return ERROR;
+	}
+	long lenght = ftell(f);
+	fclose(f);
+	return lenght;
+}
+
+int main(int argc, char **argv) {
+	if (argc < 2) {
+		printf("usage: %s <filename>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	char *filename = argv[1];
+	printf("%s:\t%ld\n", filename, size(filename));
+	return EXIT_SUCCESS;
+}
